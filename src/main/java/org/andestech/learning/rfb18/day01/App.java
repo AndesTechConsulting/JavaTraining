2f8b7cfeb999f0ac6b4d23b5051aec2dc95b9fac
andrestech <a.e.s@rambler.ru>
2018-08-12 21:37:59 +0300

    java basics commit zero

---
+++ src/main/java/org/andestech/learning/rfb18/day01/App.java
@@ -0,0 +1,10 @@
+package org.andestech.learning.rfb18.day01;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
