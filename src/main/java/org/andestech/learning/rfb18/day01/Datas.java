7b231bebac7e51259adb3f94ca01fd3f0638c3be
a0z9 <a0z9@rambler.ru>
2018-08-13 18:24:55 +0300

    ++5 add Datas class and modify mainClass attribute...

--- src/main/java/org/andestech/learning/rfb18/day01/Datas.java
+++ src/main/java/org/andestech/learning/rfb18/day01/Datas.java
@@ -2,5 +2,7 @@ package org.andestech.learning.rfb18.day01;
 
 public class Datas {
 
-    public static final int DTA_FFF_01 = 1234_4321;
+    public static final int DTA_FFF_01 = 7734_4321;
+
+    public static final int DTA_FFF_02 = 123_4321_3;
 }
